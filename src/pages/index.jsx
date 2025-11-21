22848e344aefc6dd7438c5e6eee5157a053d3f06
ketechwiz <ketechwiz@gmail.com>
2025-11-20 22:04:46 -0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,1 +1,26 @@
-export { default } from './Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Home';
+import Research from './Research';
+import Conferences from './Conferences';
+import Leadership from './Leadership';
+import Resume from './Resume';
+import Contact from './Contact';
+
+export default function Pages() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Research" element={<Research />} />
+          <Route path="/Conferences" element={<Conferences />} />
+          <Route path="/Leadership" element={<Leadership />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
